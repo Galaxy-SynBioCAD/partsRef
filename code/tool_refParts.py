6db3c32be65a71a6c1ded66e6893efba64988191
mdulac <melchior.dulac@inra.fr>
2020-04-29 12:34:03 +0200

    working and tested

--- code/tool_refParts.py
+++ code/tool_refParts.py
@@ -1,36 +1,31 @@
+#!/usr/bin/env python
+
 import sys
 import json
 import logging
 import csv
 import shutil
 
-'''
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
-    datefmt='%d-%m-%Y %H:%M:%S',
-)
-
-logging.disable(logging.INFO)
-logging.disable(logging.WARNING)
-'''
+default_parts = {'part': [{'synbiohub': 'https://synbiohub.org/public/igem/BBa_K1847014/1', 'type': 'promoter', 'name': 'PlacUV5'}, {'synbiohub': 'https://synbiohub.org/public/igem/BBa_J56012/1', 'type': 'promoter', 'name': 'Ptrc'}, {'synbiohub': 'https://synbiohub.org/public/igem/BBa_I50041/1', 'type': 'origin', 'name': 'BBR1'}, {'synbiohub': 'https://synbiohub.org/public/igem/BBa_I50032/1', 'type': 'origin', 'name': 'p15A'}, {'synbiohub': 'https://synbiohub.org/public/igem/BBa_J64101/1', 'type': 'origin', 'name': 'ColE1'}, {'synbiohub': 'https://synbiohub.org/public/igem/BBa_I13800/1', 'type': 'resistance', 'name': 'res1'}, {'synbiohub': 'https://synbiohub.org/public/igem/BBa_B1006/1', 'type': 'terminator', 'name': 'Ter'}]}
 
 if __name__ == "__main__":
     input_json_path = sys.argv[1]
+    logging.info(input_json_path)
     output_path = sys.argv[2]
+    logging.info(output_path)
     with open(input_json_path, 'r') as f:
-        input_dict = json.load(f)
+        try:
+            input_dict = json.load(f)
+        except json.decoder.JSONDecodeError:
+            input_dict = {}
         logging.info(input_dict)
-    #if the input is not empty
-    if input_dict:
-        logging.warning('Parsing the user parts')
-        with open(output_path, 'w') as infi:
-            csvfi = csv.writer(infi, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            csvfi.writerow(['Name', 'Type', 'Part'])
-            for part in input_dict:
-                csvfi.writerow([input_dict[part]['name'], input_dict[part]['type'], input_dict[part]['synbiohub']])
     #when the input is empty revert to the defaukt
-    else:
-        logging.warning('Passed empty input, sending default')
-        shutil.copy('RefParts.csv', output_path)
-
+    if not input_dict or input_dict=={'part': []}:
+        logging.warning('Empty of error reading the parts... reverting to the default')
+        input_dict = default_parts
+    #if the input is not empty
+    with open(output_path, 'w') as infi:
+        csvfi = csv.writer(infi, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csvfi.writerow(['Name', 'Type', 'Part'])
+        for part in input_dict['part']:
+            csvfi.writerow([part['name'], part['type'], part['synbiohub']])
